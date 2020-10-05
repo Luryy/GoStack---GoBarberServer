48898959b19f939c8bad15531e2c09b4d4913d2b
Lucas Yuri <lucasyuri56@hotmail.com>
2020-10-05 17:50:51 -0300

    test: create service tests

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointmentService: CreateAppointmentService;
+
+describe('CreateAppointmentService', () => {
+    beforeEach(() => {
+        fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        createAppointmentService = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+    });
+
+    it('should be able to create a new appointment', async () => {
+        const appointment = await createAppointmentService.execute({
+            date: new Date(2020, 4, 10, 14),
+            provider_id: 'provider-id',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('provider-id');
+    });
+
+    it('should not be able to create appointments in the same date', async () => {
+        const appointmentDate = new Date(2020, 4, 10, 12);
+
+        await createAppointmentService.execute({
+            date: appointmentDate,
+            provider_id: 'provider-id',
+        });
+
+        await expect(
+            createAppointmentService.execute({
+                date: appointmentDate,
+                provider_id: 'provider-id',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});

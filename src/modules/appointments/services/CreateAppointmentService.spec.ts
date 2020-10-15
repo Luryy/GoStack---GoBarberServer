6071b61cce2147c268a53b8421504a7b33b7df27
Lucas Yuri <lucasyuri56@hotmail.com>
2020-10-15 07:37:08 -0300

    test: add new test to new business core of create appointmens service

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -11,6 +11,10 @@ describe('CreateAppointmentService', () => {
         createAppointmentService = new CreateAppointmentService(
             fakeAppointmentsRepository,
         );
+
+        jest.spyOn(Date, 'now').mockImplementation(() => {
+            return new Date(2020, 4, 10, 12).getTime();
+        });
     });
 
     it('should be able to create a new appointment', async () => {
@@ -38,6 +42,44 @@ describe('CreateAppointmentService', () => {
                 date: appointmentDate,
                 provider_id: 'provider-id',
                 user_id: 'user-id',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to create appointments on past dates', async () => {
+        await expect(
+            createAppointmentService.execute({
+                date: new Date(2020, 4, 10, 11),
+                user_id: 'user',
+                provider_id: '231321312',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to create appointments with himself', async () => {
+        await expect(
+            createAppointmentService.execute({
+                date: new Date(2020, 4, 10, 17),
+                user_id: '231321312',
+                provider_id: '231321312',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to create appointments outside of commercial time', async () => {
+        await expect(
+            createAppointmentService.execute({
+                date: new Date(2020, 4, 11, 18),
+                user_id: 'user-id',
+                provider_id: 'provider-id',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+
+        await expect(
+            createAppointmentService.execute({
+                date: new Date(2020, 4, 11, 7),
+                user_id: 'user-id',
+                provider_id: 'provider-id',
             }),
         ).rejects.toBeInstanceOf(AppError);
     });

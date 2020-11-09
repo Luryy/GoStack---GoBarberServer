da1be4c4077a1416764f46a8c893d2d1351a2fb7
Lucas Yuri <lucasyuri56@hotmail.com>
2020-11-09 07:59:45 -0300

    chore: alter upload config to accept aws bucket

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,47 @@
 import path from 'path';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
-import multer from 'multer';
+
+interface IUploadConfig {
+    driver: 's3' | 'disk';
+    tmpFolder: string;
+    uploadsFolder: string;
+    multer: {
+        storage: StorageEngine;
+    };
+
+    config: {
+        // disk: {};
+        aws: {
+            bucket: string;
+        };
+    };
+}
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 const uploadsFolder = path.resolve(tmpFolder, 'uploads');
 
 export default {
+    driver: process.env.STORAGE_DRIVER,
     tmpFolder,
     uploadsFolder,
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename: (request, file, callback) => {
-            const fileHash = crypto.randomBytes(10).toString('hex');
-            const fileName = `${fileHash}-${file.originalname}`;
+    multer: {
+        storage: multer.diskStorage({
+            destination: tmpFolder,
+            filename(request, file, callback) {
+                const fileHash = crypto.randomBytes(16).toString('hex');
+                const fileName = `${fileHash}-${file.originalname}`;
 
-            return callback(null, fileName);
+                return callback(null, fileName);
+            },
+        }),
+    },
+
+    config: {
+        // disk: {},
+        aws: {
+            bucket: 'gobarber-lucasyuri',
         },
-    }),
-};
+    },
+} as IUploadConfig;

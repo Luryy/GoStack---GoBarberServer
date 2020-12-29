08195cf04ae07b6d40b8b93ec2610fa6ba35582f
Lucas Yuri <lucasyuri56@hotmail.com>
2020-12-29 16:35:30 -0300

    feat: create toast hook

--- web/src/hooks/index.tsx
+++ web/src/hooks/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import { AuthProvider } from './auth';
-// import { ToastProvider } from './toast';
+import { ToastProvider } from './toast';
 import { ThemeProvider } from './theme';
 
 const AppProvider: React.FC = ({ children }) => (
   <AuthProvider>
-    {/* <ToastProvider> */}
-    <ThemeProvider>{children}</ThemeProvider>
-    {/* </ToastProvider> */}
+    <ToastProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </ToastProvider>
   </AuthProvider>
 );
 

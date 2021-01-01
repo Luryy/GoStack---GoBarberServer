aa5c6b510f082a22b2555baf2638fc3cd891b06e
Lucas Yuri <lucasyuri56@hotmail.com>
2021-01-01 16:40:02 -0300

    fix: parse date

--- server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import { parseISO } from 'date-fns';
 
 class AppointmentsController {
     public async create(
@@ -11,11 +12,13 @@ class AppointmentsController {
         const { provider_id, date } = request.body;
         const user_id = request.user.id;
 
+        const dateParse = parseISO(date);
+
         const createAppointment = container.resolve(CreateAppointmentService);
 
         const appointment = await createAppointment.execute({
             provider_id,
-            date,
+            date: dateParse,
             user_id,
         });
 
